2a238fbb6ad1664d6911d0150ae8351766548549
RomaKun <roman@gophotoweb.ru>
2019-12-27 03:04:32 +0300

    Добавлены тесы для проверки ввода значений в инпут.

---
+++ src/test/java/HerokuAppInputTest.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class HerokuAppInputTest {
+}
